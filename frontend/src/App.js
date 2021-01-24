4eca1ef02dc3938a5b6c5930e9a728c806ca9c20
Shannon TJ <shannontj@shaw.ca>
2021-01-24 16:05:44 -0700

    move template code to react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand-name" href="index.html">
+            REACT
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
       </header>
+      <main>
+        <div class="row center">
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="./images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirt</h2>
+              </a>
+              <div class="rating">
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+                <span>
+                  <i class="fa fa-star"></i>
+                </span>
+              </div>
+              <div class="price">$29.99</div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer class="row center">All rights reserved</footer>
     </div>
   );
 }

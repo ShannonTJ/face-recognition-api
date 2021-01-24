a56e3c33ca28e6eb6ca4d0a2e7f6d73cec3c01fa
Shannon TJ <shannontj@shaw.ca>
2021-01-24 16:12:21 -0700

    data file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function App() {
   return (
-    <div class="grid-container">
-      <header class="row">
+    <div className="grid-container">
+      <header className="row">
         <div>
-          <a class="brand-name" href="index.html">
+          <a className="brand-name" href="index.html">
             REACT
           </a>
         </div>
@@ -15,38 +15,38 @@ function App() {
         </div>
       </header>
       <main>
-        <div class="row center">
-          <div class="card">
+        <div className="row center">
+          <div className="card">
             <a href="product.html">
-              <img class="medium" src="./images/p1.jpg" alt="product" />
+              <img className="medium" src="./images/p1.jpg" alt="product" />
             </a>
-            <div class="card-body">
+            <div className="card-body">
               <a href="product.html">
                 <h2>Nike Slim Shirt</h2>
               </a>
-              <div class="rating">
+              <div className="rating">
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
                 <span>
-                  <i class="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
                 </span>
               </div>
-              <div class="price">$29.99</div>
+              <div className="price">$29.99</div>
             </div>
           </div>
         </div>
       </main>
-      <footer class="row center">All rights reserved</footer>
+      <footer classNameName="row center">All rights reserved</footer>
     </div>
   );
 }

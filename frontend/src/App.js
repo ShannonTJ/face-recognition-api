f197e02682cd01eff2d9597f70b54aad5c3d53e8
Shannon TJ <shannontj@shaw.ca>
2021-01-24 16:25:51 -0700

    dynamic data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,56 @@
 import React from "react";
+import { data } from "./data";
 
 function App() {
   return (
     <div className="grid-container">
       <header className="row">
         <div>
-          <a className="brand-name" href="index.html">
+          <a className="brand-name" href="/">
             REACT
           </a>
         </div>
         <div>
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
+          <a href="/cart">Cart</a>
+          <a href="/signin">Sign In</a>
         </div>
       </header>
       <main>
         <div className="row center">
-          <div className="card">
-            <a href="product.html">
-              <img className="medium" src="./images/p1.jpg" alt="product" />
-            </a>
-            <div className="card-body">
-              <a href="product.html">
-                <h2>Nike Slim Shirt</h2>
+          {data.products.map((product) => (
+            <div key={product.id} className="card">
+              <a href={`/product/${product.id}`}>
+                <img
+                  className="medium"
+                  src={product.image}
+                  alt={product.name}
+                />
               </a>
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
+              <div className="card-body">
+                <a href={`/product/${product.id}`}>
+                  <h2>{product.name}</h2>
+                </a>
+                <div className="rating">
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                  <span>
+                    <i className="fa fa-star"></i>
+                  </span>
+                </div>
+                <div className="price">${product.price}</div>
               </div>
-              <div className="price">$29.99</div>
             </div>
-          </div>
+          ))}
         </div>
       </main>
       <footer classNameName="row center">All rights reserved</footer>

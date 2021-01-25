e8c520a04185f94bb8aefdd045d03b6e723b77ac
Shannon TJ <shannontj@shaw.ca>
2021-01-25 14:52:46 -0700

    product component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,38 +18,7 @@ function App() {
       <main>
         <div className="row center">
           {data.products.map((product) => (
-            <div key={product.id} className="card">
-              <a href={`/product/${product.id}`}>
-                <img
-                  className="medium"
-                  src={product.image}
-                  alt={product.name}
-                />
-              </a>
-              <div className="card-body">
-                <a href={`/product/${product.id}`}>
-                  <h2>{product.name}</h2>
-                </a>
-                <div className="rating">
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                  <span>
-                    <i className="fa fa-star"></i>
-                  </span>
-                </div>
-                <div className="price">${product.price}</div>
-              </div>
-            </div>
+            <Product key={product.id} product={product} />
           ))}
         </div>
       </main>

2b8c21c247228f6ec9aa08e548613ecb319435dd
Shannon TJ <shannontj@shaw.ca>
2021-01-25 15:27:12 -0700

    add react router and pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,30 @@
 import React from "react";
-import Product from "./components/Product";
-import { data } from "./data";
+import { BrowserRouter, Route } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen";
+import ProductScreen from "./pages/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand-name" href="/">
-            REACT
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {data.products.map((product) => (
-            <Product key={product.id} product={product} />
-          ))}
-        </div>
-      </main>
-      <footer className="row center">All rights reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand-name" href="/">
+              REACT
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/" component={HomeScreen} exact></Route>
+        </main>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 

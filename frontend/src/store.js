8ade0355346b91f0a05d1252920d393bedcdc3b6
Shannon TJ <shannontj@shaw.ca>
2021-01-26 15:10:48 -0700

    redux reducers added to homescreen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,11 @@
-import { compose, applyMiddleware, createStore } from "redux";
+import { compose, applyMiddleware, createStore, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import { data } from "./data";
+import { productListReducer } from "./reducers/ProductReducers";
 
 const initialState = {};
-const reducer = (state, action) => {
-  return { products: data.products };
-};
+const reducer = combineReducers({
+  productList: productListReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(

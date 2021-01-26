f064919f20ff9c19c7e46820603087f0f3f1a514
Shannon TJ <shannontj@shaw.ca>
2021-01-26 14:56:56 -0700

    add redux to project

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,17 @@
-import data from "./data";
+import { compose, applyMiddleware, createStore } from "redux";
+import thunk from "redux-thunk";
+import { data } from "./data";
 
 const initialState = {};
 const reducer = (state, action) => {
   return { products: data.products };
 };
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+export default store;

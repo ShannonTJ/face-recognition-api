30d16def17ceb5e1f1d8dbd24599d8007d22ae8e
Shannon TJ <shannontj@shaw.ca>
2021-01-25 17:42:20 -0700

    loading and message components

--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -4,19 +4,36 @@ import Product from "../components/Product";
 
 export default function HomeScreen() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/products");
-      setProducts(data);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/products");
+        setLoading(false);
+        setProducts(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
   return (
-    <div className="row center">
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
+    <div>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message>{error}</Message>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }

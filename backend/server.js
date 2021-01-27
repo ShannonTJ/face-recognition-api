6894b91217ff71babf3e3055d58087ef832d38a0
Shannon TJ <shannontj@shaw.ca>
2021-01-27 16:03:14 -0700

    test

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
+
+dotenv.config();
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {

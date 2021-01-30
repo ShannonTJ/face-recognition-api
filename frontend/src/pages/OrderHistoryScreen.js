2da6e786060757e9bd727fb0dbebcf00c7d106ce
Shannon TJ <shannontj@shaw.ca>
2021-01-30 10:49:53 -0700

    order history screen setup

--- frontend/src/pages/OrderHistoryScreen.js
+++ frontend/src/pages/OrderHistoryScreen.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Loading from "../components/Loading";
+import Message from "../components/Message";
 
 export default function OrderHistoryScreen(props) {
   const orderMineList = useSelector((state) => state.orderMineList);
@@ -8,6 +10,46 @@ export default function OrderHistoryScreen(props) {
   return (
     <div>
       <h1>Order History</h1>
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAID</th>
+              <th>DELIVERED</th>
+              <th>ACTIONS</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order) => (
+              <tr key={order._id}>
+                <td>{order._id}</td>
+                <td>{order.createdAt.substring(0, 10)}</td>
+                <td>{order.totalPrice}</td>
+                <td>{order.isPaid ? order.paidOn.substring(0, 10) : "No"}</td>
+                <td>{order.isDelivered ? order.deliveredOn : "No"}</td>
+                <td>
+                  <button
+                    type="button"
+                    className="small"
+                    onClick={() => {
+                      props.history.push(`/order/${order._id}`);
+                    }}
+                  >
+                    Details
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }

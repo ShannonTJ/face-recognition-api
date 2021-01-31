3a72f8bb22355f402540d4dbfa92053fd90c6c67
Shannon TJ <shannontj@shaw.ca>
2021-01-30 19:03:23 -0700

    deploy troubleshooting

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import bodyparser from "body-parser";
-import cors from "cors";
 import path from "path";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
-import database from "./database.js";
 
 dotenv.config();
 
 const app = express();
-//
-app.use(bodyparser.json());
-app.use(cors());
-//
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+
+mongoose
+  .connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
@@ -32,6 +33,13 @@ app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+);
+
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
@@ -39,13 +47,6 @@ app.get("/", (req, res) => {
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
-//
-app.use(express.static(path.join(__dirname, "../build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build"));
-});
-//
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
